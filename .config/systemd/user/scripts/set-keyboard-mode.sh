b2251d2bb1370ffd8d60822ed492b6416269adc1
r-mura <riccardo_mura@yahoo.com>
2022-03-06 10:15:32 +0100

    Added SystemD service for changing keybord layout dynamically

---
+++ .config/systemd/user/scripts/set-keyboard-mode.sh
@@ -0,0 +1,7 @@
+if [[ $(lsusb | grep HHKB) != "" ]]; then
+    gsettings set org.gnome.desktop.input-sources xkb-options \
+	      "['compose:rsuper', 'compose:rwin']"
+else
+    gsettings set org.gnome.desktop.input-sources xkb-options \
+	      "['lv3:ralt_switch', 'altwin:swap_alt_win', 'compose:ralt', 'ctrl:swapcaps_hyper']"
+fi
