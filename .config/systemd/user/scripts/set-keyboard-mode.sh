610eb35d18f6faf1b8311976d91c469b593b60a2
cowuake <riccardo_mura@yahoo.com>
2023-11-01 08:53:16 +0100

    Fixed HHKB switch script for systemd service

--- .config/systemd/user/scripts/set-keyboard-mode.sh
+++ .config/systemd/user/scripts/set-keyboard-mode.sh
@@ -1,7 +1,8 @@
-if [[ $(lsusb | grep HHKB) != "" ]]; then
+if [[ $(lsusb | grep 'HHKB\|PFU') != "" ]]; then
     gsettings set org.gnome.desktop.input-sources xkb-options \
 	      "['compose:rsuper', 'compose:rwin']"
 else
     gsettings set org.gnome.desktop.input-sources xkb-options \
 	      "['lv3:ralt_switch', 'altwin:swap_alt_win', 'compose:ralt', 'ctrl:swapcaps_hyper']"
 fi
+
